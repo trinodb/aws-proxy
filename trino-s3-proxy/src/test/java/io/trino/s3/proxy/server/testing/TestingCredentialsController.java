2e355f1f7bec82c64c848be2575e7f1d5064e14a
Jordan Zimmerman <jordan@jordanzimmerman.com>
2024-05-17 11:36:55 +0100

    Move testing util classes into testing package

--- trino-s3-proxy/src/test/java/io/trino/s3/proxy/server/testing/TestingCredentialsController.java
+++ trino-s3-proxy/src/test/java/io/trino/s3/proxy/server/testing/TestingCredentialsController.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.s3.proxy.server;
+package io.trino.s3.proxy.server.testing;
 
 import io.trino.s3.proxy.server.credentials.Credentials;
 import io.trino.s3.proxy.server.credentials.CredentialsController;
