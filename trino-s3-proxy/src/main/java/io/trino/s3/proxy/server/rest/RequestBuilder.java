cd060d61aea320839da9a79d5de77f60e1958d51
Jordan Zimmerman <jordan@jordanzimmerman.com>
2024-06-15 12:40:16 +0100

    Introduce RequestContent abstraction

--- trino-s3-proxy/src/main/java/io/trino/s3/proxy/server/rest/RequestBuilder.java
+++ trino-s3-proxy/src/main/java/io/trino/s3/proxy/server/rest/RequestBuilder.java
@@ -14,16 +14,25 @@
 package io.trino.s3.proxy.server.rest;
 
 import com.google.common.base.Splitter;
+import com.google.common.base.Suppliers;
+import io.trino.s3.proxy.server.rest.RequestContent.ContentType;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
+import org.apache.commons.httpclient.ChunkedInputStream;
 import org.glassfish.jersey.server.ContainerRequest;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.google.common.io.ByteStreams.toByteArray;
 import static io.trino.s3.proxy.server.collections.MultiMapHelper.lowercase;
 
 class RequestBuilder
@@ -32,8 +41,8 @@ class RequestBuilder
 
     static Request fromRequest(ContainerRequest request)
     {
-        Optional<Supplier<InputStream>> entitySupplier = request.hasEntity() ? Optional.of(request::getEntityStream) : Optional.empty();
-        return new Request(request.getRequestUri(), request.getRequestHeaders(), request.getUriInfo().getQueryParameters(), request.getMethod(), entitySupplier);
+        RequestContent requestContent = buildRequestContent(request);
+        return new Request(request.getRequestUri(), request.getRequestHeaders(), request.getUriInfo().getQueryParameters(), request.getMethod(), requestContent);
     }
 
     static ParsedS3Request fromRequest(String requestPath, ContainerRequest request, Optional<String> serverHostName)
@@ -61,5 +70,82 @@ class RequestBuilder
                     }
                     return new ParsedS3Request(parts.get(0), parts.get(1), headers, queryParameters, httpVerb, rawQuery, entitySupplier);
                 });
+    }
+
+    private static RequestContent buildRequestContent(ContainerRequest request)
+    {
+        if (!request.hasEntity()) {
+            return RequestContent.EMPTY;
+        }
+
+        ContentType contentType = switch (encoding(request.getRequestHeaders())) {
+            case "aws-chunked" -> ContentType.AWS_CHUNKED;
+            case "chunked" -> ContentType.W3C_CHUNKED;
+            default -> ContentType.STANDARD;
+        };
+
+        Supplier<InputStream> inputStreamSupplier = () -> buildInputStream(request.getEntityStream(), contentType);
+
+        Supplier<Optional<byte[]>> bytesSupplier;
+        if (contentType == ContentType.STANDARD) {
+            // memoize the entity bytes so it can be called multiple times
+            bytesSupplier = Suppliers.memoize(() -> {
+                try {
+                    return Optional.of(toByteArray(inputStreamSupplier.get()));
+                }
+                catch (IOException e) {
+                    throw new WebApplicationException(Response.Status.BAD_REQUEST);
+                }
+            });
+        }
+        else {
+            // we always stream chunked content. Never load it into memory.
+            bytesSupplier = Optional::empty;
+        }
+
+        return new RequestContent()
+        {
+            @Override
+            public ContentType contentType()
+            {
+                return contentType;
+            }
+
+            @Override
+            public Optional<byte[]> standardBytes()
+            {
+                return bytesSupplier.get();
+            }
+
+            @Override
+            public Optional<InputStream> inputStream()
+            {
+                return Optional.of(inputStreamSupplier.get());
+            }
+        };
+    }
+
+    private static InputStream buildInputStream(InputStream entityStream, ContentType contentType)
+    {
+        return (contentType == ContentType.AWS_CHUNKED) ? awsChunkedStream(entityStream) : entityStream;
+    }
+
+    private static InputStream awsChunkedStream(InputStream inputStream)
+    {
+        // TODO do we need to add a Jersey MessageBodyWriter that handles aws-chunked?
+
+        // TODO move this into a Jersey MessageBodyReader
+        try {
+            // AWS's custom chunked encoding doesn't get handled by Jersey. Do it manually.
+            return new ChunkedInputStream(inputStream);
+        }
+        catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static String encoding(MultivaluedMap<String, String> requestHeaders)
+    {
+        return firstNonNull(requestHeaders.getFirst("content-encoding"), firstNonNull(requestHeaders.getFirst("transfer-encoding"), ""));
     }
 }
