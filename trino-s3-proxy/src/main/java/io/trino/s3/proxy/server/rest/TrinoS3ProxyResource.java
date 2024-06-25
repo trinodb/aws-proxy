04e6e3bde1f699896b8edaa04570b289122d8312
Pablo Arteaga <46710067+vagaerg@users.noreply.github.com>
2024-06-25 15:55:46 +0100

    Sanity check - ensure the URL is prefixed adequately (#81)

--- trino-s3-proxy/src/main/java/io/trino/s3/proxy/server/rest/TrinoS3ProxyResource.java
+++ trino-s3-proxy/src/main/java/io/trino/s3/proxy/server/rest/TrinoS3ProxyResource.java
@@ -23,9 +23,11 @@ import jakarta.ws.rs.HEAD;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.container.AsyncResponse;
 import jakarta.ws.rs.container.Suspended;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
 import org.glassfish.jersey.server.ContainerRequest;
 
 import java.util.Optional;
@@ -126,9 +128,11 @@ public class TrinoS3ProxyResource
     private ParsedS3Request parseRequest(Request request)
     {
         String path = request.requestUri().getRawPath();
-        if (path.startsWith(s3Path)) {
-            path = path.substring(s3Path.length());
+        if (!path.startsWith(s3Path)) {
+            // Sanity check: this should never happen as this resource is prefixed at build time
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
+        path = path.substring(s3Path.length());
         if (path.isEmpty()) {
             path = "/";
         }
