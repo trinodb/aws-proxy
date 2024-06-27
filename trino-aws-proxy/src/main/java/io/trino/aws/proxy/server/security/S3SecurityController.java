d93b06850284490cd29a5173404c28bd9e095194
Jordan Zimmerman <jordan@jordanzimmerman.com>
2024-06-27 14:07:52 +0100

    Introduce database security
    Higher-level wrapper for `S3SecurityController`. Maps buckets/keys
    to database tables and database-level security.

--- trino-aws-proxy/src/main/java/io/trino/aws/proxy/server/security/S3SecurityController.java
+++ trino-aws-proxy/src/main/java/io/trino/aws/proxy/server/security/S3SecurityController.java
@@ -13,9 +13,12 @@
  */
 package io.trino.aws.proxy.server.security;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Splitter;
 import com.google.inject.Inject;
+import io.trino.aws.proxy.server.rest.RequestLoggerController;
 import io.trino.aws.proxy.spi.rest.ParsedS3Request;
+import io.trino.aws.proxy.spi.security.S3DatabaseSecurityFacadeProvider;
 import io.trino.aws.proxy.spi.security.S3SecurityFacade;
 import io.trino.aws.proxy.spi.security.S3SecurityFacadeProvider;
 import io.trino.aws.proxy.spi.security.SecurityResponse;
@@ -23,25 +26,41 @@ import io.trino.aws.proxy.spi.security.SecurityResponse;
 import java.util.Locale;
 import java.util.Optional;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 public class S3SecurityController
 {
+    private static final S3SecurityFacadeProvider DEFAULT_SECURITY_FACADE_PROVIDER = _ -> _ -> SecurityResponse.DEFAULT;
+
     private final S3SecurityFacadeProvider s3SecurityFacadeProvider;
 
     @Inject
-    public S3SecurityController(S3SecurityFacadeProvider s3SecurityFacadeProvider)
+    public S3SecurityController(Optional<S3SecurityFacadeProvider> s3SecurityFacadeProvider, Optional<S3DatabaseSecurityFacadeProvider> s3DatabaseSecurityFacadeProvider, RequestLoggerController requestLoggerController)
     {
-        this.s3SecurityFacadeProvider = requireNonNull(s3SecurityFacadeProvider, "securityFacadeProvider is null");
+        boolean hasDatabaseSecurity = s3DatabaseSecurityFacadeProvider.isPresent();
+        boolean hasNonDefaultSecurity = s3SecurityFacadeProvider.isPresent();
+
+        checkArgument(!hasDatabaseSecurity || !hasNonDefaultSecurity, "Both database security and non-default security have been bound. This is not supported.");
+
+        this.s3SecurityFacadeProvider = requireNonNull(s3SecurityFacadeProvider, "securityFacadeProvider is null")
+                .orElseGet(() -> s3DatabaseSecurityFacadeProvider.map(databaseSecurity -> (S3SecurityFacadeProvider) new S3DatabaseSecurityController(databaseSecurity, requestLoggerController))
+                        .orElse(DEFAULT_SECURITY_FACADE_PROVIDER));
     }
 
     public SecurityResponse apply(ParsedS3Request request)
     {
-        S3SecurityFacade s3SecurityFacade = s3SecurityFacadeProvider.securityFacadeForRequest(request);
+        S3SecurityFacade s3SecurityFacade = currentProvider().securityFacadeForRequest(request);
 
         Optional<String> lowercaseAction = request.rawQuery().flatMap(S3SecurityController::parseAction);
 
         return s3SecurityFacade.apply(lowercaseAction);
+    }
+
+    @VisibleForTesting
+    protected S3SecurityFacadeProvider currentProvider()
+    {
+        return s3SecurityFacadeProvider;
     }
 
     private static Optional<String> parseAction(String rawQuery)
--- trino-aws-proxy/src/test/java/io/trino/aws/proxy/server/TestS3SecurityController.java
+++ trino-aws-proxy/src/test/java/io/trino/aws/proxy/server/TestS3SecurityController.java
@@ -14,14 +14,12 @@
 package io.trino.aws.proxy.server;
 
 import com.google.inject.Inject;
-import io.trino.aws.proxy.server.testing.TestingS3SecurityFacade;
+import io.trino.aws.proxy.server.testing.TestingS3SecurityController;
 import io.trino.aws.proxy.server.testing.harness.TrinoAwsProxyTest;
 import io.trino.aws.proxy.server.testing.harness.TrinoAwsProxyTestCommonModules.WithConfiguredBuckets;
-import io.trino.aws.proxy.spi.security.S3SecurityFacadeProvider;
 import io.trino.aws.proxy.spi.security.SecurityResponse;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
@@ -36,30 +34,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @TrinoAwsProxyTest(filters = WithConfiguredBuckets.class)
-public class TestS3SecurityFacade
+public class TestS3SecurityController
 {
     private final S3Client client;
-    private final TestingS3SecurityFacade securityFacade;
-
-    private S3SecurityFacadeProvider savedDelegate;
+    private final TestingS3SecurityController securityController;
 
     @Inject
-    public TestS3SecurityFacade(S3Client client, TestingS3SecurityFacade securityFacade)
+    public TestS3SecurityController(S3Client client, TestingS3SecurityController securityController)
     {
         this.client = requireNonNull(client, "internalClient is null");
-        this.securityFacade = securityFacade;
-    }
-
-    @BeforeEach
-    public void setup()
-    {
-        savedDelegate = securityFacade.delegate();
+        this.securityController = requireNonNull(securityController, "securityFacade is null");
     }
 
     @AfterEach
     public void reset()
     {
-        securityFacade.setDelegate(savedDelegate);
+        securityController.clear();
     }
 
     @Test
@@ -74,7 +64,7 @@ public class TestS3SecurityFacade
         assertThat(listObjectsResponse.contents()).isEmpty();
 
         // set facade that disallows list objects on bucket "one"
-        securityFacade.setDelegate(request -> _ -> {
+        securityController.setDelegate(request -> _ -> {
             if ("one".equals(request.bucketName()) && request.httpVerb().equalsIgnoreCase("get")) {
                 return new SecurityResponse(false, Optional.empty());
             }
