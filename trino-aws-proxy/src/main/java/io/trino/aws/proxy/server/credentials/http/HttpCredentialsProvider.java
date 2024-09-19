a5132f77c7f051ce1596fb1acbc60201178e983c
Pablo Arteaga <parteagagonz@bloomberg.net>
2024-09-19 19:42:06 +0100

    Add configurable in-memory caching to the HTTP Credentials Provider

--- trino-aws-proxy/src/main/java/io/trino/aws/proxy/server/credentials/http/HttpCredentialsProvider.java
+++ trino-aws-proxy/src/main/java/io/trino/aws/proxy/server/credentials/http/HttpCredentialsProvider.java
@@ -13,6 +13,9 @@
  */
 package io.trino.aws.proxy.server.credentials.http;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Multimaps;
 import com.google.inject.Inject;
@@ -28,6 +31,7 @@ import jakarta.ws.rs.core.UriBuilder;
 import java.net.URI;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 import static io.airlift.http.client.FullJsonResponseHandler.createFullJsonResponseHandler;
 import static io.airlift.http.client.Request.Builder.prepareGet;
@@ -36,10 +40,20 @@ import static java.util.Objects.requireNonNull;
 public class HttpCredentialsProvider
         implements CredentialsProvider
 {
+    private record CredentialsKey(String emulatedAccessKey, Optional<String> session)
+    {
+        private CredentialsKey {
+            requireNonNull(emulatedAccessKey, "emulatedAccessKey is null");
+            requireNonNull(session, "session is null");
+        }
+    }
+
     private final HttpClient httpClient;
     private final JsonCodec<Credentials> jsonCodec;
     private final URI httpCredentialsProviderEndpoint;
     private final Map<String, String> httpHeaders;
+    private final Optional<LoadingCache<CredentialsKey, Optional<Credentials>>> credentialsCache;
+    private final Function<CredentialsKey, Optional<Credentials>> credentialsFetcher;
 
     @Inject
     public HttpCredentialsProvider(@ForHttpCredentialsProvider HttpClient httpClient, HttpCredentialsProviderConfig config, JsonCodec<Credentials> jsonCodec)
@@ -48,13 +62,39 @@ public class HttpCredentialsProvider
         this.jsonCodec = requireNonNull(jsonCodec, "jsonCodec is null");
         this.httpCredentialsProviderEndpoint = config.getEndpoint();
         this.httpHeaders = ImmutableMap.copyOf(config.getHttpHeaders());
+        if (config.getCacheSize() > 0 && config.getCacheTtl().toMillis() > 0) {
+            LoadingCache<CredentialsKey, Optional<Credentials>> cache = Caffeine.newBuilder()
+                    .maximumSize(config.getCacheSize())
+                    .expireAfterWrite(config.getCacheTtl().toJavaTime())
+                    .build(this::fetchCredentials);
+            this.credentialsCache = Optional.of(cache);
+            this.credentialsFetcher = cache::get;
+        }
+        else {
+            this.credentialsCache = Optional.empty();
+            this.credentialsFetcher = this::fetchCredentials;
+        }
     }
 
     @Override
     public Optional<Credentials> credentials(String emulatedAccessKey, Optional<String> session)
     {
-        UriBuilder uriBuilder = UriBuilder.fromUri(httpCredentialsProviderEndpoint).path(emulatedAccessKey);
-        session.ifPresent(sessionToken -> uriBuilder.queryParam("sessionToken", sessionToken));
+        return credentialsFetcher.apply(new CredentialsKey(emulatedAccessKey, session));
+    }
+
+    @VisibleForTesting
+    void resetCache()
+    {
+        credentialsCache.ifPresent(instantiatedCache -> {
+            instantiatedCache.invalidateAll();
+            instantiatedCache.cleanUp();
+        });
+    }
+
+    private Optional<Credentials> fetchCredentials(CredentialsKey credentialsKey)
+    {
+        UriBuilder uriBuilder = UriBuilder.fromUri(httpCredentialsProviderEndpoint).path(credentialsKey.emulatedAccessKey());
+        credentialsKey.session().ifPresent(sessionToken -> uriBuilder.queryParam("sessionToken", sessionToken));
         Request.Builder requestBuilder = prepareGet()
                 .addHeaders(Multimaps.forMap(httpHeaders))
                 .setUri(uriBuilder.build());
