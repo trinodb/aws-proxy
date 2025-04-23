d84be4756fb2732d8ca9dbb122d2b609a6f24b72
mosiac <mosiac@bloomberg.net>
2025-04-23 17:14:16 +0100

    Log emulated access key instead of secret key
    Unsure if this was instead for the initial development phases but we
    shouldn't be logging this now

--- trino-aws-proxy-glue/src/main/java/io/trino/aws/proxy/glue/rest/TrinoGlueResource.java
+++ trino-aws-proxy-glue/src/main/java/io/trino/aws/proxy/glue/rest/TrinoGlueResource.java
@@ -59,7 +59,7 @@ public class TrinoGlueResource
     @Produces(MediaType.APPLICATION_JSON)
     public Response gluePost(@Context Request request, @Context SigningMetadata signingMetadata, @Context RequestLoggingSession requestLoggingSession)
     {
-        requestLoggingSession.logProperty("request.glue.emulated.key", signingMetadata.credential().secretKey());
+        requestLoggingSession.logProperty("request.glue.emulated.access-key", signingMetadata.credential().accessKey());
 
         String target = request.requestHeaders().unmodifiedHeaders().getFirst("x-amz-target")
                 .orElseThrow(() -> InvalidInputException.builder().statusCode(BAD_REQUEST.getStatusCode()).build());
